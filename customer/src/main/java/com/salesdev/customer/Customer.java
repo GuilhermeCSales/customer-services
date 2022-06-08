ae52e5ff530a6c2aafa89754995ef3226926c78b
Guilherme Costa Sales <guicosal@gmail.com>
2022-06-08 15:47:12 -0300

    Database connection

--- customer/src/main/java/com/salesdev/customer/Customer.java
+++ customer/src/main/java/com/salesdev/customer/Customer.java
@@ -2,12 +2,26 @@ package com.salesdev.customer;
 
 import lombok.*;
 
-@Data
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Customer {
 
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
     private Integer id;
     private String firstName;
     private String lastName;
