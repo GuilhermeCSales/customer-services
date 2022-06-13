baf39a8bae56e53a6f2f387a9e4ee80d449ecc4a
Guilherme Costa Sales <guicosal@gmail.com>
2022-06-13 13:13:47 -0300

    Publishing Messages

--- notification/src/main/java/com/salesdev/notification/NotificationApplication.java
+++ notification/src/main/java/com/salesdev/notification/NotificationApplication.java
@@ -1,13 +1,38 @@
 package com.salesdev.notification;
 
+import com.salesdev.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.salesdev.notification",
+                "com.salesdev.amqp"
+        }
+)
 @EnableEurekaClient
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
+//    @Bean
+//    CommandLineRunner commandLineRunner(
+//            RabbitMQMessageProducer producer,
+//            NotificationConfig notificationConfig
+//    ) {
+//        return args -> {
+//            producer.publish(
+//                    new Person("John Doe", 20),
+//                    notificationConfig.getInternalExchanges(),
+//                    notificationConfig.getInternalNotificationRoutingKey()
+//            );
+//        };
+//    }
+//
+//    record Person(String name, int age) {
+//    }
 }
